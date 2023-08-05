8a128236eca5c651e60bfaa84b83ffe7f8f51b7b
Turkey690 <141506003+Turkey690@users.noreply.github.com>
2023-08-05 19:28:54 +0300

    Update config.py

--- config.py
+++ config.py
@@ -5,8 +5,8 @@ load_dotenv()
 API_ID = int(getenv("18189325"))
 API_HASH = getenv("7cb3b3ea70ba49f0721a511013eefe53")
 
-BOT_TOKEN = getenv("6540492667:AAHhF4QSufK_VHwTwxUT749r0vQBiLoAkug")
-OWNER_ID = int(getenv("5848055044"))
+BOT_TOKEN = getenv("6053302756:AAGeet_aitOe1nP7IHLM0vRFnNIcgGkBj4c")
+OWNER_ID = int(getenv("2095495680"))
 
 MONGO_DB_URI = getenv("MONGO_DB_URI")
 MUST_JOIN = getenv("MUST_JOIN", None)
